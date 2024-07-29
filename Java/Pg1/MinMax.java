2105326161e45fe7b52993626c412cada76c34f6
MalleshDarisi <malleshdarisi321@gmail.com>
2024-07-29 11:12:16 -0500

    Testing Examples

---
+++ Java/Pg1/MinMax.java
@@ -0,0 +1,21 @@
+public class MinMax{
+    public static void main(String[] args){
+        int[] arr = {1, 15, -1, 23, 45, 70, 99, -13, -5};
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++){
+            if (arr[i] < min){
+                min = arr[i];
+            }
+            if (arr[i] > max){
+                max = arr[i];
+            }
+        }
+
+        System.out.println("The min number is :" +min);
+        System.out.println("The max number is :" +max);
+
+    }
+}
---
+++ Java/Pg2/DiffMaxMin.java
@@ -0,0 +1,18 @@
+public class DiffMaxMin{
+    public static void main(String[] args){
+        int[] arr = {1, 15, -1, 23, 45, 70, 99, -13, -5};
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++){
+            if (arr[i] < min){
+                min = arr[i];
+            }
+            if (arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println("The diff is " + (max-min));
+    }
+}
